3308a9cbfc05c2375c535a0029f429cb9f255728
Bob VanderClay <bob@Victory.local>
2012-07-17 11:03:04 -0700

    Example uses extension.

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require "takitapart_framework"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -15,7 +16,6 @@ javascripts_dir = "media/js"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
